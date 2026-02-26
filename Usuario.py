2d07666852de6ec86c051a310aa7fe38551c250b
Santiago18262 <L22170783@CULIACAN.TECNM.MX>
2026-02-25 22:18:48 -0700

    Correcciones

--- Usuario.py
+++
@@ -1,7 +0,0 @@
-class Usuario:
-    def __init__(self,id,nombre):
-        self.id=id;
-        self.nombre=nombre;
-    
-    def __str__(self):
-        return f"Usuario: {self.id} Nombre: {self.nombre}"
